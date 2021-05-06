e2ee10eb0629283ab9b961e87ce536ad329908d3
BenCrafterRED <73169588+BenCrafterRED@users.noreply.github.com>
2021-05-06 19:56:04 +0200

    project setup

--- src/main/java/skyblock/main/Main.java
+++ src/main/java/skyblock/main/Main.java
@@ -1,7 +1,31 @@
 package skyblock.main;
 
+import org.apache.logging.log4j.LogManager;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
+	
+	@Override
+	public void onLoad() {
+		FileConfiguration bukkitConfig = new YamlConfiguration();
+		try {
+			LogManager.getLogger().info("Loading bukkit.yml");
+			bukkitConfig.load("bukkit.yml");
+		} catch (Exception e) {
+			LogManager.getLogger().error("Could not load bukkit.yml");
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void onEnable() {
+	}
+	
+	@Override
+	public void onDisable() {
+	}
+	
+	
 }
