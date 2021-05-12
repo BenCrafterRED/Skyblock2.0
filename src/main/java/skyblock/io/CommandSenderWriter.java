b3705eaee17f50f5cd02f03a0de2afbe8160fff8
BenCrafterRED <73169588+BenCrafterRED@users.noreply.github.com>
2021-05-12 18:58:54 +0200

    changed String.isBlank to String.isEmpty

--- src/main/java/skyblock/io/CommandSenderWriter.java
+++ src/main/java/skyblock/io/CommandSenderWriter.java
@@ -30,7 +30,7 @@ public class CommandSenderWriter extends Writer {
 	@Override
 	public void flush() throws IOException {
 		String message = buffer.toString().trim();
-		if (message.isBlank()) {
+		if (message.isEmpty()) {
 			sender.sendMessage(message);
 		}
 		buffer = new StringBuilder();
