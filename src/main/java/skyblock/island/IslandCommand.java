c6e5c1b893a7a9952496d0fbde62ee27d34a53ce
jofu0 <jofu0@DESKTOP-Klaus.speedport.ip>
2021-05-16 12:27:50 +0200

    added "/island" command

--- src/main/java/skyblock/island/IslandCommand.java
+++ src/main/java/skyblock/island/IslandCommand.java
@@ -5,10 +5,13 @@ import java.util.Arrays;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.Plugin;
 
@@ -31,22 +34,34 @@ public class IslandCommand extends Command{
 			if(player.getPersistentDataContainer().getOrDefault(new NamespacedKey(plugin,"hasIsland"), PersistentDataType.INTEGER, 0) <= 0) {
 				player.getPersistentDataContainer().set(new NamespacedKey(plugin,"hasIsland"), PersistentDataType.INTEGER, 1);
 				if(args.length == 5) {
-					IslandType type = IslandType.valueOf(args[1]);
-					Location location = player.getLocation();
-					location.setX(Integer.valueOf(args[2]));
-					location.setY(Integer.valueOf(args[3]));
-					location.setZ(Integer.valueOf(args[4]));
-					IslandCreator Ceator = new IslandCreator(plugin);
-					Ceator.createIsland(location, type);
-					player.teleport(location);
-					player.setGameMode(GameMode.SURVIVAL);
-					player.setBedSpawnLocation(location, true);
+					if(args[0].equals("create")) {
+						IslandType type = IslandType.valueOf(args[1]);
+						Location location = player.getLocation();
+						location.setX(Integer.valueOf(args[2])+0.5);
+						location.setY(Integer.valueOf(args[3])+0.5);
+						location.setZ(Integer.valueOf(args[4])+0.5);
+						IslandCreator Ceator = new IslandCreator(plugin);
+						player.setBedSpawnLocation(location, true);
+						Ceator.createIsland(location, type);
+						player.teleport(location.add(1, 0, -4));
+						player.setGameMode(GameMode.SURVIVAL);
+						
+						ItemStack item = new ItemStack(Material.WRITTEN_BOOK);
+						BookMeta Meta = (BookMeta)item.getItemMeta();
+						Meta.addPage(ChatColor.GOLD+("Wilkommen"));
+						item.setItemMeta(Meta);
+						
+						player.openBook(null);
+					}else {
+						player.sendMessage(ChatColor.RED+"The parameter " +args[0]+ " does not exist");					
+					}
 				}
 				
 				
 				
 			}else {
 				sender.sendMessage(ChatColor.RED+"You have ready an insland!");
+				player.getPersistentDataContainer().set(new NamespacedKey(plugin,"hasIsland"), PersistentDataType.INTEGER, 0);
 			}
 		}
 		return false;
